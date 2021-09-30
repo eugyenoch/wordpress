f0259c7054dd28676d06f143f69fc2178edfe409
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-09-30 14:17:30 +0100

    Create restrict_wp_admin_access.php

---
+++ restrict_wp_admin_access.php
@@ -0,0 +1,8 @@
+add_action( 'init', 'blockusers_init');
+function blockusers_init() {
+if ( is_admin() && ! current_user_can( 'administrator' ) &&
+! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+wp_redirect( home_url() );
+exit;
+}
+}
