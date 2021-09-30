5aaf178cb8186999be5e94edeac3de3165e6fce7
Eugy Enoch <38564978+eugyenoch@users.noreply.github.com>
2021-09-30 13:56:10 +0100

    Create update_wordpress_search_permalink_url.php

---
+++ update_wordpress_search_permalink_url.php
@@ -0,0 +1,7 @@
+function fb_change_search_url_rewrite() {
+	if ( is_search() && ! empty( $_GET['s'] ) ) {
+		wp_redirect( home_url( "/search/" ) . urlencode( get_query_var( 's' ) ) );
+		exit();
+	}	
+}
+add_action( 'template_redirect', 'fb_change_search_url_rewrite' );
