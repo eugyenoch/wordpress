eb5dc6d10194a17430e30cea2cb0f8bb3d276c89
David Okoli <38564978+eugyenoch@users.noreply.github.com>
2023-10-13 00:42:23 +0100

    Create add_crypto_payment_to_slicewp.php

---
+++ add_crypto_payment_to_slicewp.php
@@ -0,0 +1,40 @@
+function slicewp_custom_register_affiliate_fields_crypto_details( $fields ) {
+	
+	$fields[] = array(
+		'type'  	  		  => 'heading',
+		'default_value'		  => __( 'Cryptocurrency Payment Details; both USDC and USDT are for the Binance Smart Chain (BSC) Blockchain', 'slicewp' ),
+		'output_conditionals' => array( 'form' => array( 'affiliate_registration', 'affiliate_account' ) )
+	);
+
+	$fields[] = array(
+		'type'  	  		  => 'text',
+		'id'				  => 'slicewp-usdt-name',
+		'name'				  => 'usdt_name',
+		'label'				  => __( 'USDT Wallet', 'slicewp' ),
+		'is_required'		  => false,
+		'output_conditionals' => array( 'form' => array( 'affiliate_registration', 'affiliate_account' ) )
+	);
+
+	$fields[] = array(
+		'type'  	  		  => 'text',
+		'id'				  => 'slicewp-usdc-name',
+		'name'				  => 'usdc_name',
+		'label'				  => __( 'USDC Wallet', 'slicewp' ),
+		'is_required'		  => false,
+		'output_conditionals' => array( 'form' => array( 'affiliate_registration', 'affiliate_account' ) )
+	);
+	
+	$fields[] = array(
+		'type'  	  		  => 'text',
+		'id'				  => 'slicewp-bitcoin-address',
+		'name'				  => 'bitcoin_name',
+		'label'				  => __( 'Bitcoin address', 'slicewp' ),
+		'is_required'		  => false,
+		'output_conditionals' => array( 'form' => array( 'affiliate_registration', 'affiliate_account' ) )
+	);
+	
+
+	return $fields;
+
+}
+add_filter( 'slicewp_register_affiliate_fields', 'slicewp_custom_register_affiliate_fields_crypto_details', 50 );
