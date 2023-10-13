4fba665f578a8a24b70f6f17f83d7439defa8e82
David Okoli <38564978+eugyenoch@users.noreply.github.com>
2023-10-13 13:15:48 +0100

    Create new_woocommerce_tab_outputting_shortcode.php

---
+++ new_woocommerce_tab_outputting_shortcode.php
@@ -0,0 +1,21 @@
+function register_new_item_endpoint() {
+    add_rewrite_endpoint( 'affiliate-account', EP_ROOT | EP_PAGES );
+}
+add_action( 'init', 'register_new_item_endpoint', 10, 0);
+
+function new_item_query_vars( $vars ) {
+    $vars[] = 'affiliate-account';
+    return $vars;
+}
+add_filter( 'query_vars', 'new_item_query_vars' );
+
+function add_new_item_tab( $items ) {
+    $items['affiliate-account'] = 'Affiliate Account';
+    return $items;
+}
+add_filter( 'woocommerce_account_menu_items', 'add_new_item_tab' );
+
+function add_new_item_content() {
+    echo do_shortcode('[slicewp_affiliate_account]');
+}
+add_action( 'woocommerce_account_affiliate-account_endpoint', 'add_new_item_content' );
