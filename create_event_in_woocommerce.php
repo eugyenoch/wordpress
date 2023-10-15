adbcbe1cff83b9b6cf4f2b54e8260861569e5d83
David Okoli <38564978+eugyenoch@users.noreply.github.com>
2023-10-15 11:36:36 +0100

    Update create_event_in_woocommerce.php

--- create_event_in_woocommerce.php
+++ create_event_in_woocommerce.php
@@ -1,100 +1,52 @@
-// Register the custom product type
-function register_event_ticket_product_type() {
-    class WC_Product_Event_Ticket extends WC_Product {
-        public function __construct( $product ) {
-            $this->product_type = 'event_ticket';
-            parent::__construct( $product );
-        }
-    }
-}
-add_action( 'init', 'register_event_ticket_product_type' );
-
-// Add the custom product type to the product data dropdown
-function add_event_ticket_product_type( $types ) {
-    // Include "virtual" and "downloadable" options
-    $types['event_ticket'] = array(
-        'label'       => __('Event Ticket', 'woocommerce'),
-        'description' => __('Sell tickets for events.', 'woocommerce'),
-        'supports'    => array(
-            'virtual',
-            'downloadable',
-        ),
-    );
-    return $types;
-}
-add_filter( 'product_type_selector', 'add_event_ticket_product_type' );
-
 // Modify the product data tabs to include custom fields
-function add_event_ticket_product_data_tabs( $tabs ) {
-	// Include the "General" and "Inventory" tabs
-    $tabs['general'] = array(
-        'label'  => __('General', 'woocommerce'),
-        'target' => 'general_product_data',
-    );
-    $tabs['inventory'] = array(
-        'label'  => __('Inventory', 'woocommerce'),
-        'target' => 'inventory_product_data',
-    );
+function add_event_ticket_product_data_tabs($tabs){
     $tabs['event_ticket'] = array(
-        'label'  => __( 'Event Ticket', 'woocommerce' ),
+        'label'  => __('Event Ticket', 'woocommerce'),
         'target' => 'event_ticket_options',
+        'class'  => array('show_if_event_ticket'),
     );
     return $tabs;
 }
-add_filter( 'woocommerce_product_data_tabs', 'add_event_ticket_product_data_tabs' );
+add_filter('woocommerce_product_data_tabs', 'add_event_ticket_product_data_tabs', 20, 1);
 
-// Render custom fields on the product data tabs
-function render_event_ticket_product_data_fields() {
+// Show general and inventory tabs for event_ticket type
+function show_general_and_inventory_settings(){
     global $post;
-    ?>
-    <div id="event_ticket_options" class="panel woocommerce_options_panel">
-        <div class="options_group">
-            <?php
-            woocommerce_wp_text_input(
-                array(
-                    'id'          => '_event_date',
-                    'label'       => __( 'Event Date', 'woocommerce' ),
-                    'placeholder' => 'YYYY-MM-DD',
-                    'desc_tip'    => 'true',
-                )
-            );
-
-            woocommerce_wp_text_input(
-                array(
-                    'id'          => '_event_location',
-                    'label'       => __( 'Event Location', 'woocommerce' ),
-                    'placeholder' => __( 'Enter the event location', 'woocommerce' ),
-                    'desc_tip'    => 'true',
-                )
-            );
-		woocommerce_wp_text_input( array(
-        'id'          => 'event_time',
-        'label'       => __( 'Event Time (GMT)', 'woocommerce' ),
-        'placeholder' => __( 'Enter the event time in GMT', 'woocommerce' ),
-    ) );
+    $product_type = get_post_meta($post->ID, '_product_type', true);
     
-    	woocommerce_wp_textarea_input( array(
-        'id'          => 'event_speakers',
-        'label'       => __( 'Event Speakers', 'woocommerce' ),
-        'placeholder' => __( 'Enter the speakers for this event', 'woocommerce' ),
-    ) );
-            ?>
-        </div>
-    </div>
-    <?php
+    if ($product_type === 'event_ticket') {
+        echo '<div id="general_product_data" class="panel woocommerce_options_panel">';
+        do_action('woocommerce_product_options_general_product_data');
+        echo '</div>';
+        
+        echo '<div id="inventory_product_data" class="panel woocommerce_options_panel show_if_simple">';
+        do_action('woocommerce_product_options_inventory_product_data');
+        echo '</div>';
+    }
 }
-add_action( 'woocommerce_product_data_panels', 'render_event_ticket_product_data_fields' );
+add_action('woocommerce_product_data_panels', 'show_general_and_inventory_settings');
 
-// Save custom field data upon product save
-function save_event_ticket_product_data_fields( $post_id ) {
-    $event_date = ! empty( $_POST['_event_date'] ) ? sanitize_text_field( $_POST['_event_date'] ) : '';
-    $event_location = ! empty( $_POST['_event_location'] ) ? sanitize_text_field( $_POST['_event_location'] ) : '';
-	$event_time    = isset( $_POST['event_time'] ) ? sanitize_text_field( $_POST['event_time'] ) : '';
-    $event_speakers = isset( $_POST['event_speakers'] ) ? sanitize_textarea_field( $_POST['event_speakers'] ) : '';
-
-    update_post_meta( $post_id, '_event_date', $event_date );
-    update_post_meta( $post_id, '_event_location', $event_location );
-	update_post_meta( $product_id, 'event_time', $event_time );
-    update_post_meta( $product_id, 'event_speakers', $event_speakers );
+// Show virtual and downloadable options
+function add_virtual_downloadable_options(){
+    global $post;
+    $product_type = get_post_meta($post->ID, '_product_type', true);
+    
+    if ($product_type === 'event_ticket') {
+        woocommerce_wp_checkbox(
+            array(
+                'id'          => '_virtual',
+                'label'       => __('Virtual', 'woocommerce'),
+                'description' => __('Enable this if the event is virtual.', 'woocommerce'),
+            )
+        );
+        
+        woocommerce_wp_checkbox(
+            array(
+                'id'          => '_downloadable',
+                'label'       => __('Downloadable', 'woocommerce'),
+                'description' => __('Enable this if the event has downloadable content.', 'woocommerce'),
+            )
+        );
+    }
 }
-add_action( 'woocommerce_process_product_meta_event_ticket', 'save_event_ticket_product_data_fields' );
+add_action('woocommerce_product_options_general_product_data', 'add_virtual_downloadable_options');
