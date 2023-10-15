9ac8c046f5eaa6dcf5310e299418ec9a26eb3b9a
David Okoli <38564978+eugyenoch@users.noreply.github.com>
2023-10-15 11:05:25 +0100

    Update adds_a_new_kind_of_virtual_and_downloadable_product_to_woocommerce.php

--- adds_a_new_kind_of_virtual_and_downloadable_product_to_woocommerce.php
+++ adds_a_new_kind_of_virtual_and_downloadable_product_to_woocommerce.php
@@ -1,20 +1,20 @@
-// Register the ticket product type
-function add_ticket_product_type() {
+// Register the subscription product type
+function add_subscription_product_type() {
     class WC_Product_Subscription extends WC_Product {
         public function __construct( $product ) {
-            $this->product_type = 'ticket';
+            $this->product_type = 'subscription';
             parent::__construct( $product );
         }
     }
 }
-add_action( 'init', 'add_ticket_product_type' );
+add_action( 'init', 'add_subscription_product_type' );
 
 // Set the subscription product type in the product data
-function set_ticket_product_data( $data ) {
-    if ( 'ticket' === $data['product-type'] ) {
-        $data['virtual'] = 'yes'; // Set whether the ticket is a virtual product or not
-        $data['downloadable'] = 'no'; // Set whether the ticket is downloadable or not
+function set_subscription_product_data( $data ) {
+    if ( 'subscription' === $data['product-type'] ) {
+        $data['virtual'] = 'yes'; // Set whether the subscription is a virtual product or not
+        $data['downloadable'] = 'no'; // Set whether the subscription is downloadable or not
     }
     return $data;
 }
-add_filter( 'woocommerce_product_data', 'set_ticket_product_data' );
+add_filter( 'woocommerce_product_data', 'set_subscription_product_data' );
